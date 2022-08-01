823d26a4a18e6e3a64fd1354aa6674bc9e1a0688
Ryan Nazareth <ryankarlos@gmail.com>
2022-08-01 06:08:08 +0100

    readme update

--- src/create-schema-graph.py
+++ src/create-schema-graph.py
@@ -1,5 +1,5 @@
 from sqlalchemy_schemadisplay import create_schema_graph
 from sqlalchemy import MetaData
 
-graph = create_schema_graph(metadata=MetaData('postgres://user:pwd@host/database'))
+graph = create_schema_graph(metadata=MetaData('postgresql://hello_fastapi:hello_fastapi@db/hello_fastapi_dev'))
 graph.write_png('my_erd.png')
