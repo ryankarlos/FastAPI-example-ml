293987e2008bd8d908ca68e09eb75f493cff777d
Ryan Nazareth <ryankarlos@gmail.com>
2022-08-05 04:11:38 +0100

    fix flake 8

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -18,7 +18,9 @@ class GetClientAge(BaseModel):
     def check_all_params_should_not_be_none(cls, values):
         gender, education, marriage = values.get("gender"), values.get("education"), values.get("marriage")
         if gender is None and education is None and marriage is None:
-            raise ValueError("At least one of gender, education, marriage query strings must be passed with a value")
+            raise ValueError(
+                "At least one of gender, education, marriage " "query strings must be passed with a value"
+            )
         return values
 
 
